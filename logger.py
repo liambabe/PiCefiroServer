0e45eecfa9df0977fc5d21b9a5a7825700848783
liambabe <liambabes13@gmail.com>
2020-03-05 11:24:24 +1300

    logger

--- logger.py
+++ logger.py
@@ -2,14 +2,17 @@ from enum import Enum
 from typing import TextIO
 import datetime
 
-#enum to catagorize log types
 class LogType(Enum):
+    """ Enum to catagorize log types """
+
     ClientRequest = 1
     ClientResponce = 2
     ServerRequest = 3
     ServerResponce = 4
     ServerAction = 5
     Error = 6
+    LoggerStart = 7
+    LoggerEnd = 8
 
 class Logger:
 
@@ -21,10 +24,19 @@ class Logger:
 
     def __enter__(self):
         self.__logFileWriter = open(self.__logFileName, "a")
+        self.__logClearStartLine()
+        self.log(LogType.LoggerStart, "Start Of Server Session")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.log(LogType.LoggerEnd, "End Of Server Session")
         self.__logFileWriter.close()
     
     def log(self, type: LogType, text: str):
         logString = str(datetime.datetime.now()) + ", " + type._name_ + ", " + text + "\n"
         self.__logFileWriter.write(logString)
+
+    def __logClearStartLine(self):
+        """  """
+        
+        self.__logFileWriter.write("\n")
+        self.__logFileWriter.write("============================")
