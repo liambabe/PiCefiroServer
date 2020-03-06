59d5b4a4813316da4cdc7f1e5f99bb57d69049a3
liambabe <liambabes13@gmail.com>
2020-03-06 23:13:32 +1300

    small radiator edit

--- Modules/radiatorfancontroller.py
+++ Modules/radiatorfancontroller.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from Modules.carmodule import CarModule
 
 class PIController:
@@ -5,6 +7,10 @@ class PIController:
     I = 0
 
 class RadiatorFanController(CarModule):
+
+    piController: Type[PIController]
     
     def __init__(self):
         super(RadiatorFanController, self).__init__("Radiator Fan Controller")
+
+        self.piController = PIController()
