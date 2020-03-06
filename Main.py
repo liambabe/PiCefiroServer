cc523cdb65db2d93d33faecffdb5eb09fe7abbd6
liambabe <liambabes13@gmail.com>
2020-03-06 22:03:52 +1300

    setting up the basics

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from logger import Logger, LogType
 from Modules.modulemanager import ModuleManager
 
 serverLogger = Logger("LogFile.txt")
-moduleManager = ModuleManager()
+moduleManager = ModuleManager(serverLogger)
 
 with serverLogger:
     server = Server(serverLogger)
--- Modules/modulemanager.py
+++ Modules/modulemanager.py
@@ -1,13 +1,20 @@
 from typing import Type
 
+from logger import Logger, LogType
 from Modules.carmodule import CarModule
 from Modules.intercoolerspray import IntercoolerSpray
-
+from Modules.radiatorfancontroller import RadiatorFanController
 
 class ModuleManager:
 
+    serverLogger: Type[Logger]
+
     InterCoolerSprayModule: Type[CarModule]
+    RadiatorFanControllerModule: Type[CarModule]
 
+    def __init__(self, logger: Type[Logger]):
+        self.serverLogger = logger
 
-    def __init__(self):
+        self.serverLogger.log(LogType.ModuleAction, "Module manager Starting")
         self.InterCoolerSprayModule = IntercoolerSpray()
+        self.RadiatorFanControllerModule = RadiatorFanController()
