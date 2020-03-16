62359e7be7baac1c5a6f3b91a1673e6e7140af16
liambabe <liambabes13@gmail.com>
2020-03-17 06:28:45 +1300

    refacting code to be usuable

--- Modules/oledcontroller.py
+++ Modules/oledcontroller.py
@@ -16,14 +16,21 @@ class OledController(CarModule):
         super(OledController, self).__init__("Oled Controller")
         self.WIDTH = 128
         self.HEIGHT = 64
-        self.BORDER = 5
+        self.BORDER = 4
 
         self.i2c = board.I2C()
         self.oled = adafruit_ssd1306.SSD1306_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3c)
+        self.font = ImageFont.load_default()
 
+        self.clear()
+        self.initScreen()
+        self.displayText("test")
+     
+    def clear(self):
         self.oled.fill(0)
         self.oled.show()
 
+    def initScreen(self):
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
         self.image = Image.new('1', (self.oled.width, self.oled.height))
@@ -37,20 +44,15 @@ class OledController(CarModule):
         # Draw a smaller inner rectangle
         self.draw.rectangle((self.BORDER, self.BORDER, self.oled.width - self.BORDER - 1, self.oled.height - self.BORDER - 1),
                    outline=0, fill=0)
-     
-        # Load default font.
-        self.font = ImageFont.load_default()
-     
+
+    def displayText(self, displayText: str):
         # Draw Some Text
-        self.text = "Hello World!"
-        (font_width, font_height) = self.font.getsize(self.text)
+        text = displayText
+        (font_width, font_height) = self.font.getsize(text)
         self.draw.text((self.oled.width//2 - font_width//2, self.oled.height//2 - font_height//2),
-        self.text, font=self.font, fill=255)
+        text, font=self.font, fill=255)
      
         # Display image
         self.oled.image(self.image)
         self.oled.show()
-     
-    def clear(self):
-        self.oled.fill(0)
-        self.oled.show()
+
